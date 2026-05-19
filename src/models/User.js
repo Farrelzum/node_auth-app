ec56d988104a6e0ea6e08ed1105cda57c774e69a
Grzegorz <grze.pra@gmail.com>
2026-05-19 19:40:58 +0200

    chore: define User model

---
+++ src/models/User.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+const User = sequelize.define('User', {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    validate: { isEmail: true },
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  isActive: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    defaultValue: false,
+  },
+  activationToken: {
+    type: DataTypes.STRING,
+    unique: true,
+  },
+});
+
+module.exports = User;
