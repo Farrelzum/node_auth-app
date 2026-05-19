d43326b067c090fb3fa324ba7d20b9674ea0c979
Grzegorz <grze.pra@gmail.com>
2026-05-19 18:33:37 +0200

    chore: initialize project architecture and express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const express = require('express');
+
+const app = express();
+
+app.use(express.json({ limit: '1mb' }));
+
+app.get('/api/status', (req, res) => {
+  res.json({ status: 'The API is functioning correctly.' });
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('The server is listening on port 3000');
+});
