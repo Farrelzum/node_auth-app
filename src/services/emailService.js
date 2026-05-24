4baa8d3a19e6bda1c45bbb689d77e23dece8be32
Grzegorz <grze.pra@gmail.com>
2026-05-24 19:48:12 +0200

    feat: add password reset email function

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -12,13 +12,15 @@ const transporter = nodemailer.createTransport({
 });
 
 const sendActivationEmail = async (to, token) => {
+  const baseUrl = process.env.APP_URL || 'http://localhost:3000';
+
   try {
     const info = await transporter.sendMail({
       from: '"Example Team" <team@example.com>',
       to: to,
       subject: 'Activation link',
-      text: `To activate your account click the link: http://localhost:3000/api/auth/activate/${token}`,
-      html: `<a href="http://localhost:3000/api/auth/activate/${token}">Click here to activate your account</a>`,
+      text: `To activate your account click the link: ${baseUrl}/api/auth/activate/${token}`,
+      html: `<a href="${baseUrl}/api/auth/activate/${token}">Click here to activate your account</a>`,
     });
 
     // eslint-disable-next-line no-console
@@ -29,4 +31,27 @@ const sendActivationEmail = async (to, token) => {
   }
 };
 
-module.exports = sendActivationEmail;
+const sendPasswordResetEmail = async (to, token) => {
+  const baseUrl = process.env.APP_URL || 'http://localhost:3000';
+
+  try {
+    const info = await transporter.sendMail({
+      from: '"Example Team" <team@example.com>',
+      to: to,
+      subject: 'Password Reset Request',
+      text: `To reset your password account click on this link: ${baseUrl}/api/auth/reset-password/${token}`,
+      html: `<a href="${baseUrl}/api/auth/reset-password/${token}">Click here to reset your password</a>`,
+    });
+
+    // eslint-disable-next-line no-console
+    console.log('Message sent: %s', info.messageId);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error('Error while sending mail:', err);
+  }
+};
+
+module.exports = {
+  sendActivationEmail,
+  sendPasswordResetEmail,
+};
