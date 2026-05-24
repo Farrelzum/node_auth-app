b7e86db8d2d8853823561e7038bac82860ec3268
Grzegorz <grze.pra@gmail.com>
2026-05-24 18:25:26 +0200

    feat: add user name update endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
+const User = require('../models/User');
 
 router.get('/profile', authMiddleware, async (req, res) => {
   return res.status(200).json(req.user);
+});
+
+router.patch('/name', authMiddleware, async (req, res) => {
+  try {
+    const { name } = req.body;
+
+    if (!name) {
+      return res.status(400).json({ message: 'Lack of name in request' });
+    }
+
+    const user = await User.findByPk(req.user.id);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    user.name = name;
+
+    await user.save();
+
+    return res
+      .status(200)
+      .json({ message: 'Name has been changed successfully' });
+  } catch (error) {
+    return res.status(500).json({ message: 'Server error' });
+  }
 });
 
 module.exports = router;
